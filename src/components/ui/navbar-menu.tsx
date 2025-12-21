c664befe9fb339403ba0d0ac52ae7f38d0e4e35e
Yash Samar <yashsamar@Yashs-MacBook-Air.local>
2025-12-21 11:31:44 +0530

    fix: framer-motion transition type error

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -4,10 +4,10 @@ import { motion } from "motion/react";
 
 
 const transition = {
-  type: "spring",
-  mass: 0.5,
-  damping: 11.5,
-  stiffness: 100,
+  type: "spring" as const,
+  mass: 1,
+  damping: 15,
+  stiffness: 200,
   restDelta: 0.001,
   restSpeed: 0.001,
 };
